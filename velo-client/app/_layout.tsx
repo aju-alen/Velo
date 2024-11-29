f11eec64f0d7796db1d9540f58cf5d4774169e7d
aju-alen <alenaju1304@gmail.com>
2024-11-29 17:27:54 +0400

    root layout splash

--- velo-client/app/_layout.tsx
+++ velo-client/app/_layout.tsx
@@ -1,13 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useFonts } from 'expo-font';
-import { Stack, Redirect } from 'expo-router';
-import { View } from 'react-native';
-
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { PaperProvider } from 'react-native-paper';
 import StripeProviderWrapper from '@/components/StripeProviderWrapper';
-import { SplashScreen } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -17,54 +16,33 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     Montserrat: require('../assets/fonts/Montserrat-Regular.ttf'),
+    // Raleway: require('../assets/fonts/Raleway-Regular.ttf'),
+    // SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-  const [appIsReady, setAppIsReady] = useState(false);
+  
 
   useEffect(() => {
-    async function prepare() {
-      try {
-        // Simulate an artificial delay for a better user experience.
-        await new Promise((resolve) => setTimeout(resolve, 3000)); // 3-second delay
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-
     if (loaded) {
-      prepare();
+      SplashScreen.hideAsync();
     }
   }, [loaded]);
 
-  // Hide splash screen when app is ready
-  useEffect(() => {
-    if (appIsReady) {
-      SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!loaded || !appIsReady) {
-    return null; // Keep the splash screen visible until fonts are loaded and delay is complete
+  if (!loaded) {
+    return null;
   }
 
   return (
-    <StripeProviderWrapper>
-      <PaperProvider>
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <View style={{ flex: 1 }}>
-            <Stack>
-              <Stack.Screen name="index" options={{ headerShown: false }} />
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found" />
-            </Stack>
-
-            {/* Place redirect logic outside the stack */}
-            <Redirect href="/" />
-          </View>
-        </ThemeProvider>
-      </PaperProvider>
+      <StripeProviderWrapper>
+    <PaperProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false}} />
+      <Stack.Screen name="+not-found" />
+      </Stack>
+    </ThemeProvider>
+    </PaperProvider>
     </StripeProviderWrapper>
   );
 }
